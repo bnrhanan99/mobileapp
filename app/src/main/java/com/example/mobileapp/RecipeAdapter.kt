c34f96f8ca0d272325b8823fbefba288ee05e94d
bnrhanan99 <bnr.hanan@gmail.com>
2024-12-22 23:06:04 +0100

    fourth commit

--- app/src/main/java/com/example/mobileapp/RecipeAdapter.kt
+++ app/src/main/java/com/example/mobileapp/RecipeAdapter.kt
@@ -27,7 +27,6 @@ class RecipeAdapter(
         val recipe = recipes[position]
         holder.bind(recipe)
 
-        // Use the correct context here for Toast
         holder.itemView.setOnClickListener {
             Toast.makeText(context, "Clicked on ${recipe.name}", Toast.LENGTH_SHORT).show()
         }
